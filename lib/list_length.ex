4527494bc2f37145d5d3f08bbf41d42e8483aed1
Bruno de Oliveira Felipe <bruno.felipe7170@gmail.com>
2021-07-09 18:21:40 -0300

    Count the size of a list recursively

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: length(list, 0)
+
+  defp length([], counter) do
+    counter
+  end
+
+  defp length([_head | tail], counter) do
+    counter = counter + 1
+    length(tail, counter)
   end
 end
